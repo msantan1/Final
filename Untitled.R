f2d92f4ca8415d63fedfb885df51f5e02776ba95
msantan1 <msantan14@gmail.com>
2016-03-16 20:25:04 -0700

    kljads;kf

---
+++ Untitled.R
@@ -0,0 +1,10 @@
+git remote add origin https://github.com/msantan1/Final.git
+git push -u origin master
+
+git ewenharrison/test.git rename https://github.com/msantan1/Final.git github
+git remote add origin <https://github.com/msantan1/Final.gitL>
+  # test and make sure the DreamHost origin works properly for fetch and push
+  git remote rm github
+
+git config --global user.name "msantan1"
+git config --global user.email "msantan14@gmail.com"
